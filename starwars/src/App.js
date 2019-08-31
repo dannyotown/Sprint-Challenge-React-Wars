0f8f45242565213c58b7c97f9d2cca7d64e5d3f3
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-08-31 12:53:01 -0500

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import { CharCard } from './components/CharacterCards/CharacterCards';
+import axios from "axios";
+import styled from 'styled-components';
+
+const BodyContain = styled.div`
+    margin: 0 auto;
+    margin-top: 2%;
+    width: 80%;
+    margin-bottom: 2%;
+`;
+const FlexDiv = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  const [charNames, updateCharNames] = useState([]);
+  useEffect(()=>{
+    axios.get('https://swapi.co/api/people/')
+    .then(res=>updateCharNames(res.data.results))
+    .catch(err=>console.log(err))
+  },[]);
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <BodyContain>
+      <FlexDiv>
+        {charNames.map((charName, index)=>{
+           return <CharCard name={charName.name} height={charName.height} weight={charName.mass}
+           haircolor={charName.hair_color} skincolor ={charName.skin_color} gender={charName.gender}
+           key={index}></CharCard>;
+        })}
+        </FlexDiv>
+      </BodyContain>
     </div>
   );
 }
---
+++ starwars/src/components/CharacterCards/CharacterCards.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+const CharacterCard = styled.div`
+    margin: 0 auto;
+    width: 30%;
+    height: 75%;
+    text-align:center; 
+    border: white 1px solid;
+    background-color: #443e3e;
+    color: white;
+    font-size: 1 rem;
+    margin-top: 2%;
+    opacity: 0.7;
+    padding: 1%;
+    border-radius: 5px;
+`;
+const CharacterName = styled.h1`
+    font-weight: bold;
+    font-size: 1.5rem;
+`;
+const NewH3 = styled(CharacterName)`
+    font-size: 1.25 rem;
+    text-align: Center;
+`;
+const Attributes = styled.p`
+    text-align: left;
+    font-size: 1rem;
+    margin-left: 5%;
+`;
+const Span = styled.span`
+    font-weight: bold;
+`;
+export function CharCard(props) {
+    return (
+            <CharacterCard>
+                <CharacterName>
+                    <p>{props.name}</p>
+                </CharacterName>
+                <hr/>
+                <NewH3>Attributes</NewH3>
+                    <Attributes>
+                        <Span>Gender: </Span>{props.gender}
+                    </Attributes>
+                    <Attributes>
+                        <Span>Height: </Span>{props.height}
+                    </Attributes>
+                    <Attributes>
+                        <Span>Weight: </Span>{props.weight}
+                    </Attributes>
+                    <Attributes>
+                        <Span>Hair Color: </Span>{props.haircolor}
+                    </Attributes>
+                    <Attributes>
+                        <Span>Skin Color: </Span>{props.skincolor}
+                    </Attributes>
+            </CharacterCard>
+    );
+}
